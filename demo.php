d1725edf952b7361a6cf2a169851e783a6b2876c
tormundgerhardsen <tormund.gerhardsen@gmail.com>
2019-10-08 11:11:23 +0200

    Add demo

---
+++ demo.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once(__DIR__.'/vendor/autoload.php');
+
+use Apility\T4SevenOffice\T4SevenOffice;
+
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+
+
+/* ----------------------------------------------------- */
+
+
+T4SevenOffice::setCredentials(
+  $_ENV['USERNAME'],
+  $_ENV['PASSWORD'],
+  $_ENV['APPLICATION_ID'],
+  $_ENV['IDENTITY_ID']
+);
+
+// By default, this is set to false
+T4SevenOffice::setUsePhpSession(true);
+
+try {
+  
+  T4SevenOffice::authenticateService();
+  
+  var_dump(T4SevenOffice::getSessionId());
+  
+  $companyService = T4SevenOffice::companyService();
+  
+  $companies = $companyService->GetCompanies([
+    'searchParams' => ['CompanyName' => 'Test'],
+    'returnProperties' => ['Name', 'Id']
+  ]);
+  
+  var_dump($companies);
+  
+  
+} catch (Exception $e) {
+  die('Exception: '.$e->getMessage());
+}
